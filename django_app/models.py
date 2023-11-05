5c3784af6abf3a47084c5591fc46d0b74b170c10
groot85 <queiroz.analaura@gmail.com>
2023-11-05 15:50:49 -0300

    recomeçar do zero

--- django_app/models.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-from django.db import models
-from django.utils import timezone
-
-
--- django_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("<h1>Olá mundo, testando a visu do Django nesse domingão de sol!</h1>")
