96dc0aab0a95b474df557e7c5b141f28a4634177
groot85 <queiroz.analaura@gmail.com>
2023-11-12 14:55:16 -0300

    css e js - criacao das pastas e inst bootstrap

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # A view is a place where we put the "logic" of our application
-def post_list (request):
-    return render (request, 'blog/post_list.html',{})
-
 def inicio (request):
-    return HttpResponse ("oieeeee")
+    return render(request,'inicio.html')
+
+def post_list (request):
+    return render (request, 'post_list.html')
