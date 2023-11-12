d860818e44a20e4a51fbd73e928b4bb20122ab0c
groot85 <queiroz.analaura@gmail.com>
2023-11-12 13:34:34 -0300

    testando exemplos  de inicio e paginas de visuali

--- anaqueiroz/urls.py
+++ anaqueiroz/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ('', inicio),
     path('', include ('blog.urls')),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 # A view is a place where we put the "logic" of our application
 def post_list (request):
     return render (request, 'blog/post_list.html',{})
+
+def inicio (request):
+    return HttpResponse ("oieeeee")
