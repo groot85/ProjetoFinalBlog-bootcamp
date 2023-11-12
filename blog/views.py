d91e0fecefd597a49f0716068c15c8388509e886
groot85 <queiroz.analaura@gmail.com>
2023-11-12 10:33:42 -0300

    Criando a pagina modelo do blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+# A view is a place where we put the "logic" of our application
+def post_list (request):
+    return render (request, 'blog/post_list.html',{})
