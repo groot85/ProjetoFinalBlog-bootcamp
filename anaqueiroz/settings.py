cfd91322c08ca3c4e19264a72fda2a50b0e38e6d
groot85 <queiroz.analaura@gmail.com>
2023-11-14 17:28:53 -0300

    atualizando com a Isa

--- anaqueiroz/settings.py
+++ anaqueiroz/settings.py
@@ -126,3 +126,4 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#so para ter alguma mudanca
