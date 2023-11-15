3b34af97f80e087126b742651f2728add71ce7b3
groot85 <queiroz.analaura@gmail.com>
2023-11-15 18:54:06 -0300

    alteracao template inicio e ord publ

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
+from django.views import generic
 
+#publicacao com resumo da postagem (ler mais em: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Generic_views)
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_date')
+    template_name = 'index.html'
+
+class DetailView #>>>>>> continuar fazendo <<<<
+
+"""
 # A view is a place where we put the "logic" of our application
 def inicio (request):
     return render(request,'inicio.html')
 
-def post_list (request):
+def post_list ():
     return render (request, 'post_list.html',)
 
 #https://docs.djangoproject.com/en/3.2/ref/models/querysets/
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+"""
