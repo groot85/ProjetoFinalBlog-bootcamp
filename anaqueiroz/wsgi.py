f919a144522e7efb4d744cd837440ec3b09f3680
groot85 <queiroz.analaura@gmail.com>
2023-11-26 18:49:20 -0300

    edit wsgi

--- anaqueiroz/wsgi.py
+++ anaqueiroz/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
 import os
+import sys
 
+# assuming your django settings file is at '/home/username/mysite'
+# and your manage.py is is at '/home/amaanx/mysite/manage.py'
+path = '/home/anaqueiroz/anaqueiroz' 
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'anaqueiroz.settings' 
+
+# then:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anaqueiroz.settings")
-
 application = get_wsgi_application()
